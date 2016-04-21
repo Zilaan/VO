e1fc3bc96bc6c7d50c0aa717b575beff43f3bc31
Raman Haddad <raman.haddad@gmail.com>
2016-04-21 12:43:52 +0200

    Created basic structure for Odometry.h

--- odometry/Odometry.h
+++ odometry/Odometry.h
@@ -4,21 +4,28 @@
 class Odometry
 {
 public:
-	struct parameters
+	struct odometryParameters
 	{
 		/*
 		 * Some parameters used
 		 * for the visual odometry
 		 * i.e. focus length etc.
 		 */
-		parameters ()
+		int test;
+		odometryParameters()
 		{
 			/*
 			 * Deafault values for
 			 * parameters from above
 			 */
+			test = 0;
 		}
 	};
+	struct parameters
+	{
+		Odometry::odometryParameters odParam;
+		Matcher::parameters maParam;
+	}
 
 	// Constructor, takes as inpute a parameter structure
 	Odometry(parameters param);
@@ -26,8 +33,25 @@ public:
 	// Deconstructor
 	virtual ~Odometry();
 
+	void process(const cv::Mat &image);
+
 private:
-	/* data */
+	// Estimate motion
+	void estimateMotion();
+
+	// Paramters used
+	parameters param;
+	Matcher *mainMatcher;
+
+	bool firstRun;
+	std::vector<KeyPoint> *p_keypoints;
+	std::vector<KeyPoint> *c_keypoints;
+	std::vector<KeyPoint> *swap_keypoints;
+	std::vector<DMatch> *good_matches;
+	cv::Mat *c_descriptors;
+	cv::Mat *p_descriptors;
+	cv::Mat *swap_descriptors;
+	bool firstRun = true;
 };
 
 #endif // ODOMETRY_H
