9ff651d036345d8d821dc429451b357e2eadbcb1
Raman Haddad <raman.haddad@gmail.com>
2016-04-21 15:18:25 +0200

    Add body blala for odometry.cpp

--- odometry/src/Odometry.cpp
+++ odometry/src/Odometry.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+#include "Odometry.h"
+#include "/Users/Raman/Documents/Programmering/opencv/VO/FeatureDetection/include/Matcher.h"
+#include <opencv2/core/core.hpp>
 
 using namespace std;
 using namespace cv;
 
-Odometry::Odometry(parameters param) : param(param)
+Odometry::Odometry(parameters param) : param(param), firstRun(true)
 {
 	mainMatcher = new Matcher(param.maParam);
 }
@@ -17,16 +21,16 @@ Odometry::~Odometry()
  */
 void Odometry::process(const Mat &image)
 {
-	if(firstRun)
+	if(true)
 	{
 		// Only one frame available
-		mainMatcher->fastMatcher(image, p_keypoints, p_descriptor);
+		mainMatcher->fastMatcher(image, *p_keypoints, *p_descriptors);
 		firstRun = false;
 	}
 	else
 	{
-		mainMatcher->fastMatcher(image, p_keypoints, p_descriptor,
-								 c_keypoints, c_descriptor, good_matches);
+		mainMatcher->fastMatcher(image, *p_keypoints, *p_descriptors,
+								 *c_keypoints, *c_descriptors, *good_matches);
 		swap_keypoints = p_keypoints;
 		p_keypoints = c_keypoints;
 		c_keypoints = swap_keypoints;
