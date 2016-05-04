88c3467607851357a2a0b3172ecf770b150d3bbb
Raman Haddad <raman.haddad@gmail.com>
2016-05-04 09:17:37 +0200

    Reformat Scale with astyle

--- odometry/src/Scale.cpp
+++ odometry/src/Scale.cpp
@@ -8,36 +8,37 @@ using namespace std;
 
 float sigma_h(vector<Point3f> q)//Function to calculate sigma_h for the skewed Gaussian kernel
 {
-        vector<float> res;
-        if ( q.empty() ) return 0;
-        else    {
-                for ( vector<Point3f>::iterator it = q.begin(); it != q.end(); ++it )
-                {
-                        res.push_back( sqrt( pow( it->x, 2  ) + pow ( it->y, 2 ) + pow ( it->z, 2 )) );
-                }
+	vector<float> res;
+	if ( q.empty() ) return 0;
+	else
+	{
+		for ( vector<Point3f>::iterator it = q.begin(); it != q.end(); ++it )
+		{
+			res.push_back( sqrt( pow( it->x, 2  ) + pow ( it->y, 2 ) + pow ( it->z, 2 )) );
+		}
 
-                if ( res.size() % 2 == 0 )
-                        return ( 0.01 * (res[res.size()/2 - 1] + res[res.size()/2]) );
-                else
-                        return ( 0.02 * res[res.size()/2] );
-        }
+		if ( res.size() % 2 == 0 )
+			return ( 0.01 * (res[res.size() / 2 - 1] + res[res.size() / 2]) );
+		else
+			return ( 0.02 * res[res.size() / 2] );
+	}
 }
 
 
 float skew_gauss_kernel(float height, vector<Point3f> xyz)//function to estimate the height of the camera
 {
-        float sig_h = sigma_h( xyz );
-        vector<float> val;
-        vector<float>::iterator pos;
+	float sig_h = sigma_h( xyz );
+	vector<float> val;
+	vector<float>::iterator pos;
 
-        for( vector<Point3f>::iterator it = xyz.begin(); it != xyz.end(); ++it )
-        {
-                if ( height - it->y > 0)
-                        val.push_back( exp( (-0.5*pow( it->y, 2 ) )/pow( sig_h, 2 ) ) );
-                else
-                        val.push_back( exp( (-0.5*pow( it->y,2 ) )/pow( sig_h*0.01, 2 ) ) );
-        }
-        pos = max_element ( val.begin(), val.end() );
+	for( vector<Point3f>::iterator it = xyz.begin(); it != xyz.end(); ++it )
+	{
+		if ( height - it->y > 0)
+			val.push_back( exp( (-0.5 * pow( it->y, 2 ) ) / pow( sig_h, 2 ) ) );
+		else
+			val.push_back( exp( (-0.5 * pow( it->y, 2 ) ) / pow( sig_h * 0.01, 2 ) ) );
+	}
+	pos = max_element ( val.begin(), val.end() );
 
-        return *pos;
+	return *pos;
 }
